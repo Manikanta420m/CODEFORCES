da7791c11a3209960a5fea737f34e3d896fe787f
ManiKanta <manikanta420m@gmail.com>
2025-01-18 06:13:16 +0530

    Add files via upload

---
+++ B. Number Factorization.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+
+#define ll           long long
+#define PI           3.1415926535897932384626
+#define rep(i,n)     for(int i=0;i<n;i++)
+#define repk(i,k,n)  for(int i=k;i<n;i++)
+#define pb           push_back
+#define vec          vector<int>
+#define u_map        unordered_map<int,int>
+#define vecp         vector<pair<int,int>>
+#define ff           first 
+#define ss           second
+#define yes          cout << "YES" << endl;
+#define cout_ans     cout<<ans<<endl;
+#define no           cout << "NO" << endl;
+#define all(v)       (v.begin),b.end();
+#define sortall(v)   sort(all(v));
+
+
+const int N = 1e5;
+const int INF = int(1e9) + 99;
+const int MAXI = 20001;
+const int MAXN = 1e5 + 4;
+const int MOD1 = 998244353;
+const ll mod=998244353; 
+
+/*******************************cons****************************/
+bool prime(int x){
+    for(int i=2;i*i<=x;i++){if(x%i==0){return 0;}}
+    return 1;
+}
+ll gcd(int a,int b)
+{
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+ll lcm(ll a[],ll n){
+    ll r=a[0];
+    for(ll i=1;i<n;i++){r=((a[i]*r)/gcd(a[i],r));}
+    return r;
+}
+ll mypow(ll a,ll b){
+    ll res=1LL;while(b){
+        if(b&1) res=res*a%mod;
+        a=a*a%mod;b>>=1;
+    }return res;
+}
+ll fact(ll m){
+    if(m==0 or m==1){return 1;}
+    else{return m*fact(m-1);}
+}
+ll set_bits(ll h)
+{
+    ll c=0;
+    while(h){c+=(h&1);h>>=1;}
+    return c;
+}
+
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+
+void solve(){ 
+    ll n,c,ans=0;
+    cin>>n;
+    unordered_map<ll,ll> mp;
+    for(ll i=2;i*i<=n;i++){
+       c=0;
+       while(n%i==0){
+              n/=i;
+              c++;
+              if(!mp.count(c))mp[c]=i;
+              else mp[c]*=i;
+       }
+    }
+    if(n!=1){
+        if(!mp.count(1))mp[1]=n;
+        else mp[1]*=n;
+    }
+    for(auto i:mp)ans+=i.ss;
+    cout<<ans;
+}
+
+int main(){
+     w(x){
+        solve();
+        cout<<endl;}  }
